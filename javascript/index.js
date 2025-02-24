eefeaf2e8b5918294719ed4e5df43d79c26b882b
Niflheim-ai <67445829+Niflheim-ai@users.noreply.github.com>
2025-02-24 21:50:01 +0800

    v1.1
    Finished front-end of homepage

--- javascript/index.js
+++ javascript/index.js
@@ -17,4 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 500);
         }
     });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        grabCursor: true,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay:3000,
+            disableOnInteraction: false
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 });
