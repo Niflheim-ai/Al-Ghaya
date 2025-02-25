26061e246bcb3b7798a853b5af59af03e4a5d45a
Niflheim-ai <67445829+Niflheim-ai@users.noreply.github.com>
2025-02-25 15:24:47 +0800

    Login and Registration UI/UX

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    menuToggle.addEventListener("click", function () {
-        if (mobileMenu.classList.contains("hidden")) {
-            mobileMenu.classList.remove("hidden");
-            setTimeout(() => {
-                mobileMenu.classList.remove("max-h-0");
-                mobileMenu.classList.add("max-h-[300px]");
-            }, 10);
-        } else {
-            mobileMenu.classList.remove("max-h-[300px]");
-            mobileMenu.classList.add("max-h-0");
-            setTimeout(() => {
-                mobileMenu.classList.add("hidden");
-            }, 500);
-        }
-    });
-
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 20,
