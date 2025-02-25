508d38f85e8bf52fc3894b442f9b60f0899470c9
Niflheim-ai <67445829+Niflheim-ai@users.noreply.github.com>
2025-02-25 15:26:32 +0800

    -- IGNORE --
    for vercel deployment

--- html/index.js
+++ html/index.js
@@ -1,21 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    menuToggle.addEventListener("click", function () {
-        if (mobileMenu.classList.contains("hidden")) {
-            mobileMenu.classList.remove("hidden");
-            setTimeout(() => {
-                mobileMenu.style.maxHeight = mobileMenu.scrollHeight + "px";
-            }, 10);
-        } else {
-            mobileMenu.style.maxHeight = "0px";
-            setTimeout(() => {
-                mobileMenu.classList.add("hidden");
-            }, 900);
-        }
-    });
-
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 20,
